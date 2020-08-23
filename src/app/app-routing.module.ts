b5ab7cacea83989c104b95075bf8941b9d6fc22e
Anthony Malerba <toshy_malerba@hotmail.com>
2020-08-23 16:58:23 +0200

    ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './security/login-page/login-page.component';
+import { DummyPageComponent } from './dummy-page/dummy-page.component';
+import { AuthGuard } from './security/guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Add this default route to redirect to dummy
+  { path: "", redirectTo: "dummy", pathMatch: "full" },
+  { path: "login", component: LoginPageComponent },
+  // Add the route to display the dummy page
+  {
+    path: "dummy",
+    component: DummyPageComponent,
+    // Prevent access to this page to unauthenticated users
+    canActivate: [AuthGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

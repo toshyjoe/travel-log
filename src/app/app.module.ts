d44a6be828144b5480028696c96eb3cf7a6756a2
Anthony Malerba <toshy_malerba@hotmail.com>
2020-08-23 16:53:43 +0200

    first commit app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ApiTokenInterceptorService } from "./api/api-token-interceptor.service";
+import { SecurityModule } from './security/security.module';
+import { DummyPageComponent } from './dummy-page/dummy-page.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DummyPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule, 
+    SecurityModule
   ],
-  providers: [],
+  providers: [ 
+    {
+      provide: HTTP_INTERCEPTORS, //Now all your API calls will have the Authorization header when the user is logged in.
+      useClass: ApiTokenInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
